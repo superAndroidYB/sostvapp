be3e989b6287ac80db2ba1eca1bd8c6669b6c5ae
superAndroidYB <18509220676@163.com>
2016-07-05 22:58:13 +0800

    关于我们

--- app/src/main/java/activity/sostv/com/ui/AboutUsActivity.java
+++ app/src/main/java/activity/sostv/com/ui/AboutUsActivity.java
@@ -2,17 +2,52 @@ package activity.sostv.com.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import activity.sostv.com.sostvapp.R;
 import io.bxbxbai.common.utils.GlobalExecutor;
 
 public class AboutUsActivity extends BaseActivity {
 
+    protected Typeface pingfang;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        setTitle(R.string.aboutUs);
+        pingfang = Typeface.createFromAsset(this.getAssets(), "fonts/PingFangHeavy.ttf");
+        setPingfangByTextView();
+    }
+
+    private void setPingfangByTextView() {
+        View view = this.getWindow().getDecorView();
+        List<View> views = getAllChildViews(view);
+        for (View v : views){
+            if(v instanceof TextView){
+                ((TextView)v).setTypeface(pingfang);
+            }
+        }
+    }
+
+    private List<View> getAllChildViews(View view) {
+        List<View> allchildren = new ArrayList<View>();
+        if (view instanceof ViewGroup) {
+            ViewGroup vp = (ViewGroup) view;
+            for (int i = 0; i < vp.getChildCount(); i++) {
+                View viewchild = vp.getChildAt(i);
+                allchildren.add(viewchild);
+                allchildren.addAll(getAllChildViews(viewchild));
+            }
+        }
+        return allchildren;
     }
 
     public static void start(final Context context) {
