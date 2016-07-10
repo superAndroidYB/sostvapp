f518f6648437109cd54f2657b569d53766eb3544
superAndroidYB <18509220676@163.com>
2016-07-10 14:26:05 +0800

    系列讲座视频界面

--- app/src/main/java/activity/sostv/com/ui/VideoListActivity.java
+++ app/src/main/java/activity/sostv/com/ui/VideoListActivity.java
@@ -1,15 +1,32 @@
 package activity.sostv.com.ui;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import activity.sostv.com.sostvapp.R;
 
-public class VideoListActivity extends AppCompatActivity {
+public class VideoListActivity extends BaseActivity {
 
+    private String videoType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_list);
+
+        parseIntent(getIntent());
     }
+
+    private void parseIntent(Intent i){
+        videoType = (String)i.getStringExtra("VIDEO_TYPE");
+    }
+
+    public static void start(Context context, String videoType){
+        Intent intent = new Intent(context, VideoListActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("VIDEO_TYPE",videoType);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
+
 }
--- app/src/main/java/activity/sostv/com/ui/VideoXLListActivity.java
+++ app/src/main/java/activity/sostv/com/ui/VideoXLListActivity.java
@@ -1,73 +1,87 @@
 package activity.sostv.com.ui;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import activity.sostv.com.adapter.SosHomeZxtjGridViewAdapter;
-import activity.sostv.com.global.Constants;
-import activity.sostv.com.global.WebServiceHelper;
-import activity.sostv.com.model.SosVideo;
-import activity.sostv.com.model.SostvRequest;
-import activity.sostv.com.model.SostvResponse;
 import activity.sostv.com.sostvapp.R;
-import activity.sostv.com.widght.SostvGridView;
-import io.bxbxbai.common.T;
+import activity.sostv.com.widght.ParallaxListView;
 
 public class VideoXLListActivity extends BaseActivity {
 
-	@ViewInject(R.id.sostvXlVideoView)
-	private SostvGridView gridView;
-	
-	private SosHomeZxtjGridViewAdapter adapter;
-	
-	private List<SosVideo> datas = null;
-	
+
+	private String[] dataTitle = new String[]{
+			"但以理视频讲座","启示录视频讲座","圣经预言与时事征兆","半小时布道","5分钟漫步	","罗马书讲座","加拉太书讲座","希伯来书讲座",
+			"圣所，福音唯一的蓝图","罗马书简明讲解"
+	};
+	@ViewInject(R.id.xl_video_listview)
+	private ParallaxListView videoListView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_xl_videoview);
 		ViewUtils.inject(this);
-		initToolBar();
 		setTitle(R.string.btn_xl);
-		loadData();
+
+		initUI();
 	}
-	
-	private void loadData(){
-		datas = new ArrayList<SosVideo>();
-		adapter = new SosHomeZxtjGridViewAdapter(this, datas);
-		gridView.setAdapter(adapter);
-		
-		LoadData loadData = new LoadData();
-		SostvRequest request = new SostvRequest();
-		request.setProperty(0, Constants.VIDEO_XLJZ);
-		request.setProperty(1, "VideoXLListActivity");
-		loadData.execute(request);
+
+	private void initUI(){
+		final View headerView = LayoutInflater.from(this).inflate(R.layout.xllist_header_view,null);
+		final ImageView img = (ImageView)headerView.findViewById(R.id.parallax_img);
+
+		videoListView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+		videoListView.addHeaderView(headerView);
+		videoListView.setAdapter(new XlListViewAdapter());
+
+		headerView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+			@Override
+			public void onGlobalLayout() {
+				videoListView.setParallaxImage(img);
+				headerView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+			}
+		});
 	}
-	
-	private class LoadData extends WebServiceHelper {
+
+	class XlListViewAdapter extends BaseAdapter{
 
 		@Override
-		protected void onPostExecute(SostvResponse result) {
-			if(result == null){
-				return;
-			}
-			if(!Constants.CODE.equals(result.getReturnCode())){
-				T.showToast("服务器内部错误,请稍后重试");
-				return;
-			}
-			String data = result.getReturnData();
-			Gson gson = new Gson();
-			List<SosVideo> list = gson.fromJson(data, new TypeToken<List<SosVideo>>(){}.getType());
-			datas.addAll(0,list);
-			adapter.notifyDataSetChanged();
-			
+		public int getCount() {
+			return dataTitle.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return dataTitle[position];
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = LayoutInflater.from(VideoXLListActivity.this).inflate(R.layout.xl_video_view_item,null);
+			ImageView itemImage = (ImageView)v.findViewById(R.id.xl_item_image);
+			TextView itemText = (TextView)v.findViewById(R.id.xl_item_text);
+			itemText.setText(dataTitle[position]);
+			return v;
 		}
 	}
+
+
+
+
+	
+
 }
